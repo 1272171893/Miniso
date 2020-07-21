179b2d6cf4bd31e6b7b2eba3a4a1389761f93a7f
“1272171893 <123456q1272171893@qq.com>
2020-07-21 22:56:28 +0800

    按钮的基础逻辑

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import React from 'react';
+import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>hello</Button>
+      <Button disabled>hello</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>hello</Button>
+
     </div>
   );
 }
