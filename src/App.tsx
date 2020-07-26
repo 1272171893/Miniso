a5a7ab07e093028f9611e170aafd04bf0dc98bf4
“1272171893 <123456q1272171893@qq.com>
2020-07-26 14:43:22 +0800

    样式修改

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
       <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>hello</Button>
       <br/>
-      <Menu defaultIndex={0} onSelect={(index)=>alert(index)}>
+      <Menu defaultIndex={0} mode="vertical" onSelect={(index)=>alert(index)}>
         <MenuItem index={0}>cool link1</MenuItem>
         <MenuItem disabled index={1}>cool link2</MenuItem>
         <MenuItem index={2}>cool link3</MenuItem>
