2dec48bbcbef9de896ecbd71877c0a1b7e82d078
“1272171893 <123456q1272171893@qq.com>
2020-07-26 15:48:15 +0800

    单元测试

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -14,9 +14,9 @@ const testVerProps: MenuProps = {
 const generateMenu = (props:MenuProps) => {
   return (
     <Menu {...props}>
-      <MenuItem index={0}>active</MenuItem>
-      <MenuItem disabled index={1}>disabled</MenuItem>
-      <MenuItem index={2}>cool link3</MenuItem>
+      <MenuItem >active</MenuItem>
+      <MenuItem disabled >disabled</MenuItem>
+      <MenuItem >cool link3</MenuItem>
     </Menu>
   );
 };
--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,5 +1,6 @@
 import React, { useState, createContext } from "react";
 import classNames from "classnames";
+import { MenuItemProps } from "./menuItem";
 type MenuMode = "horizontal" | "vertical";
 type SelectCallback = (selectedIndex: number) => void;
 export interface MenuProps {
@@ -30,10 +31,20 @@ const Menu: React.FC<MenuProps> = (props) => {
     index: active ? active : 0,
     onSelect: handleClick,
   };
+  const renderChildren = () => {
+    return React.Children.map(children, (child, index) => {
+      const childElement = child as React.FunctionComponentElement<MenuItemProps>;
+      const { displayName } = childElement.type;
+      if(displayName==="MenuItem"){
+        return React.cloneElement(childElement,{index});
+      }
+      console.error("warning:Menu has a child which is not a MenuItem component")
+    });
+  };
   return (
     <ul className={classes} style={style} data-testid="test-menu">
       <MenuContext.Provider value={passedContext}>
-        {children}
+        {renderChildren()}
       </MenuContext.Provider>
     </ul>
   );
