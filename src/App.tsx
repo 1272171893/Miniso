720de6dec91622d77f85689867b5c0be6dd4db1a
“1272171893 <123456q1272171893@qq.com>
2020-07-26 12:48:04 +0800

    menu基础

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
   return (
@@ -10,7 +12,12 @@ function App() {
       <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>hello</Button>
       <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
       <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>hello</Button>
-
+      <br/>
+      <Menu defaultIndex={0}>
+        <MenuItem>cool link1</MenuItem>
+        <MenuItem>cool link2</MenuItem>
+        <MenuItem>cool link3</MenuItem>
+      </Menu>
     </div>
   );
 }
