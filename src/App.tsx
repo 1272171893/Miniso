49e6c1a22810cb7ca799f58e3983530ffc6c1877
“1272171893 <123456q1272171893@qq.com>
2020-07-26 14:22:55 +0800

    menu的继续封装

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ function App() {
       <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
       <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>hello</Button>
       <br/>
-      <Menu defaultIndex={0}>
-        <MenuItem>cool link1</MenuItem>
-        <MenuItem>cool link2</MenuItem>
-        <MenuItem>cool link3</MenuItem>
+      <Menu defaultIndex={0} onSelect={(index)=>alert(index)}>
+        <MenuItem index={0}>cool link1</MenuItem>
+        <MenuItem disabled index={1}>cool link2</MenuItem>
+        <MenuItem index={2}>cool link3</MenuItem>
       </Menu>
     </div>
   );
