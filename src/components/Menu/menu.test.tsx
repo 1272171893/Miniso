5a4c5ca8267e9eb259cbe844f13ee1f270377535
“1272171893 <123456q1272171893@qq.com>
2020-08-02 01:54:24 +0800

    添加测试

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { render, RenderResult, fireEvent, cleanup } from "@testing-library/react";
+import { render, RenderResult, fireEvent, cleanup, wait } from "@testing-library/react";
 import Menu, { MenuProps } from "./menu";
 import MenuItem from "./menuItem";
+import SubMenu from "./subMenu";
 const testProps: MenuProps = {
   defaultIndex: "0",
   onSelect: jest.fn(),
@@ -17,9 +18,26 @@ const generateMenu = (props:MenuProps) => {
       <MenuItem >active</MenuItem>
       <MenuItem disabled >disabled</MenuItem>
       <MenuItem >cool link3</MenuItem>
+      <SubMenu title="dropdown">
+          <MenuItem>dropdown1</MenuItem>
+        </SubMenu>
     </Menu>
   );
 };
+
+const createStyleFile =()=>{
+  const cssFile:string =`
+  .miniso-submenu {
+    display:none;
+  }
+  .miniso-submenu.menu-opened{
+    display:block;
+  }`
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.innerHTML = cssFile;
+  return style;
+}
 let wrapper:RenderResult, 
     menuElement:HTMLElement, 
     activeElement:HTMLElement, 
@@ -27,6 +45,7 @@ let wrapper:RenderResult,
 describe("test Menu and MenuItem Components", () => {
   beforeEach(()=>{
     wrapper = render(generateMenu(testProps));
+    wrapper.container.append(createStyleFile());
     menuElement =wrapper.getByTestId('test-menu');
     activeElement =wrapper.getByText('active');
     disabledElement =wrapper.getByText('disabled');
@@ -34,7 +53,7 @@ describe("test Menu and MenuItem Components", () => {
   it("should render corrent Menu and MenuItem based on default props", () => {
       expect(menuElement).toBeInTheDocument();
       expect(menuElement).toHaveClass('miniso-menu test');
-      expect(menuElement.getElementsByTagName('li').length).toEqual(3);
+      expect(menuElement.querySelectorAll(":scope > li").length).toEqual(4);
       expect(activeElement).toHaveClass('menu-item is-active');
       expect(disabledElement).toHaveClass('menu-item is-disabled');
   });
@@ -43,7 +62,7 @@ describe("test Menu and MenuItem Components", () => {
       fireEvent.click(thirdItem);
       expect(thirdItem).toHaveClass('is-active');
       expect(activeElement).not.toHaveClass('is-active');
-      expect(testProps.onSelect).toHaveBeenCalledWith(2);
+      expect(testProps.onSelect).toHaveBeenCalledWith("2");
       fireEvent.click(disabledElement);
       expect(disabledElement).not.toHaveClass("is-active");
       expect(testProps.onSelect).not.toHaveBeenCalledWith(1);
@@ -54,4 +73,18 @@ describe("test Menu and MenuItem Components", () => {
       menuElement =wrapper.getByTestId('test-menu');
       expect(menuElement).toHaveClass("menu-vertical");
   });
+  it("should show dropdown items when hover on subMenu",async()=>{
+    expect(wrapper.queryByText('dropdown1')).not.toBeVisible();
+    const dropElement = wrapper.getByText('dropdown');
+    fireEvent.mouseEnter(dropElement);
+    await wait (()=>{
+      expect(wrapper.queryByText('dropdown1')).toBeVisible();
+    })
+    fireEvent.click(wrapper.getByText('dropdown1'));
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0');
+    fireEvent.mouseLeave(dropElement);
+    await wait (()=>{
+      expect(wrapper.queryByText('dropdown1')).not.toBeVisible();
+    })
+  })
 });
