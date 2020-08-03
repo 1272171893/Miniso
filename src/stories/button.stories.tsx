fcb022894bbf2131b0cd838522689ba5ce3947c4
“1272171893 <123456q1272171893@qq.com>
2020-08-03 22:26:12 +0800

    添加样式入口

--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -2,5 +2,25 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import Button from "../components/Button/button";
-const defaultButton = ()=>(<Button onClick={action("clicked")}>default button</Button>)
-storiesOf("Button component",module).add("默认 Button",defaultButton)
+const defaultButton = () => (
+  <Button onClick={action("clicked")}>default button</Button>
+);
+const ButtonWithSize = () => (
+  <>
+    <Button size="lg">default button</Button>
+    <Button size="sm">default button</Button>
+  </>
+);
+const ButtonWithType = () => (
+  <>
+    <Button btnType="primary">primary button</Button>
+    <Button btnType="danger">danger button</Button>
+    <Button btnType="link" href="https://www.baidu.com">
+      link button
+    </Button>
+  </>
+);
+storiesOf("Button component", module)
+  .add("默认 Button", defaultButton)
+  .add("不同size Button", ButtonWithSize)
+  .add("不同type Button", ButtonWithType);
