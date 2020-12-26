def9fa4afb6a28058387a2b7bfb5f16d73cc8e81
1272171893 <1272171893@qq.com>
2020-12-26 14:49:09 +0800

    input初步功能实现

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -1,31 +1,33 @@
-import React, { FC } from "react";
+import React, { FC, InputHTMLAttributes, AnchorHTMLAttributes } from "react";
 import classNames from "classnames";
 export enum InputType {
-    input = "input",
-    TextArea = "textArea",
+  input = "input",
+  TextArea = "textarea",
 }
 export enum InputSize {
-    Large = "lg",
-    Small = "sm",
+  Large = "lg",
+  Small = "sm",
 }
 
-interface InputProps {
-    className?: string;
-    disabled?: boolean;
-    type?: InputType;
-    size?: InputSize;
-    placeholder?: string;
+interface InputPop {
+  className?: string;
+  disabled?: boolean;
+  type?: InputType;
+  size?: InputSize;
+  placeholder?: string;
+  defaultValue?: string | undefined;
+  value?: string | undefined;
 }
+type NativeButtonProps = InputPop & InputHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = InputPop & AnchorHTMLAttributes<HTMLElement>;
+export type InputProps = Partial<NativeButtonProps & AnchorButtonProps>;
 export const Input: FC<InputProps> = (props) => {
-    const { type = "input", className, size, disabled, ...restProps } = props;
-    const classes = classNames("miniso-input", className, {
-        [`input-${type}`]: type,
-        [`btn-${size}`]: size,
-        disabled: disabled,
-    });
-
-    return (
-        <input className={classes} type="input" {...restProps}></input>
-    );
+  const { type = "input", className, size, disabled, ...restProps } = props;
+  const classes = classNames("miniso-input", className, {
+    [`input-${type}`]: type,
+    [`btn-${size}`]: size,
+    disabled: disabled,
+  });
+  return <input className={classes} type="input" {...restProps}></input>;
 };
 export default Input;
