6d99b506986907cd27f3b45fea820da614dc8034
1272171893 <1272171893@qq.com>
2020-12-26 10:49:02 +0800

    修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
@@ -10,24 +10,25 @@ library.add(fas);
 function App() {
   return (
     <div className="App">
-      {/* <FontAwesomeIcon icon={faCoffee} size="lg"></FontAwesomeIcon> */}
-      <Icon icon="arrow-down" size="lg" theme="info"></Icon>
-      <Button autoFocus>hello</Button>
-      <Button disabled>hello</Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
-      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>hello</Button>
-      <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
-      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>hello</Button>
-      <br/>
-      <Menu defaultIndex="0" mode="vertical" onSelect={(index)=>alert(index)} defaultOpenSubMenus={["3"]}>
-        <MenuItem >cool link1</MenuItem>
-        <MenuItem disabled >cool link2</MenuItem>
-        <MenuItem >cool link3</MenuItem>
-        <SubMenu title="dropdown">
-          <MenuItem>dropdown1</MenuItem>
-          <MenuItem>dropdown2</MenuItem>
-        </SubMenu>
-      </Menu>
+      <Fragment>
+        <Icon icon="arrow-down" size="lg" theme="info"></Icon>
+        <Button autoFocus>hello</Button>
+        <Button disabled>hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>hello</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">hello</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>hello</Button>
+        <br />
+        <Menu defaultIndex="0" mode="vertical" onSelect={(index) => alert(index)} defaultOpenSubMenus={["3"]}>
+          <MenuItem >cool link1</MenuItem>
+          <MenuItem disabled >cool link2</MenuItem>
+          <MenuItem >cool link3</MenuItem>
+          <SubMenu title="dropdown">
+            <MenuItem>dropdown1</MenuItem>
+            <MenuItem>dropdown2</MenuItem>
+          </SubMenu>
+        </Menu>
+      </Fragment>
     </div>
   );
 }
